b378a839a46ac0ad72b9afd5cd2b82dcb64f6932
jesse.lin <hsiao.jung.lin@gmail.com>
2023-02-04 11:58:58 +0800

    * Update generate_udev_net_rules

--- generate_udev_net_rules.sh
+++ generate_udev_net_rules.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 rulesConfig=/etc/udev/rules.d/70-persistent-net.rules
+exec_lspci=/usr/bin/lspci
+exec_reboot=/usr/sbin/reboot
+exec_awk=/usr/bin/awk
+exec_egrep=/usr/bin/egrep
+exec_echo=/usr/bin/echo
+exec_cat=/usr/bin/echo
 
 if [ -f $rulesConfig ] ; then
 	exit 0
@@ -12,16 +18,18 @@ addToUdevRules() {
 	pciId=$1
 	hostNic=$2
 	gustNic=$3
-	echo "# HostNic = ${hostNic}" >> $rulesConfig
-	echo "SUBSYSTEM==\"net\", ACTION==\"add\", KERNELS==\"${pciId}\", DRIVERS==\"?*\", ATTR{type}==\"1\", KERNEL==\"ens*\", NAME=\"${guestNic}\"\n" >> $rulesConfig
+	${exec_echo} "# HostNic = ${hostNic}" >> $rulesConfig
+	${exec_echo} "SUBSYSTEM==\"net\", ACTION==\"add\", KERNELS==\"${pciId}\", DRIVERS==\"?*\", ATTR{type}==\"1\", KERNEL==\"ens*\", NAME=\"${guestNic}\"\n" >> $rulesConfig
 }
 
-for bus_name in $(lspci | egrep -i 'network|ethernet' | awk '{printf "0000:%s\n",$1}')
+for bus_name in $(${exec_lspci} | ${exec_egrep} -i 'network|ethernet' | ${exec_awk} '{printf "0000:%s\n",$1}')
 do
 	dir=${pci_dir}/${bus_name}
 
-	hostNic=$(cat ${dir}/label})
+	hostNic=$(${exec_cat} ${dir}/label})
 	hostNic="Ethernet6"
-	guestNic=$(echo "eth"${hostNic##Ethernet})
+	guestNic=$(${exec_echo} "eth"${hostNic##Ethernet})
 	addToUdevRules ${bus_name} ${hostNic} ${guestNic}	
 done
+
+${exec_reboot}
